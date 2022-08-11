762174d0ee859a4d8562cfa70b59ae4d2d45e851
Chillmoon <108537775+Chillmoon@users.noreply.github.com>
2022-08-11 18:02:06 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hi, this is my Dictionary app</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
