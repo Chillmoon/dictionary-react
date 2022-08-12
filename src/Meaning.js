e6a56176724417a6692cbe525603dd7ee6849b74
Chillmoon <108537775+Chillmoon@users.noreply.github.com>
2022-08-12 19:28:58 +0300

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -12,11 +13,10 @@ export default function Meaning(props) {
             </p>
 
             <em>{definition.example}</em>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
-
-      <hr />
     </div>
   );
 }
