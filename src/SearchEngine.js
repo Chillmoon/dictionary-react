e3d71789a4decb4657c3011e659cf089ea06d0e1
Chillmoon <108537775+Chillmoon@users.noreply.github.com>
2022-08-13 23:58:24 +0300

    added photo section

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function SearchEngine(props) {
   let [keyword, setKeyword] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function handleSubmit(event) {
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     event.preventDefault();
     axios.get(url).then(handleResponse);
+    const pexelsKey =
+      "563492ad6f9170000100000152303c3e19f447db923ce1e623ed2b50";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsUrl, {
+        headers: {
+          Authorization: `token ${pexelsKey}`,
+        },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -37,6 +53,7 @@ export default function SearchEngine(props) {
       </form>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
